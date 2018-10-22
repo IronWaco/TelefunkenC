25e18981b928fd08722db1b6e705cd4241bc04a9
IronWaco <wmagarioscalla@gmail.com>
2018-10-22 18:24:45 -0400

    Clase Juego

---
+++ src/Juego.java
@@ -0,0 +1,39 @@
+
+public class Juego {
+	private int numJugadores;
+	private Jugador ju[]=new  Jugador[10];
+	private int ronda;
+	
+	public Juego(int numJugadores, Jugador[] ju, int ronda) {
+		super();
+		this.numJugadores = numJugadores;
+		this.ju = ju;
+		this.ronda = ronda;
+	}
+
+	public int getNumJugadores() {
+		return numJugadores;
+	}
+
+	public void setNumJugadores(int numJugadores) {
+		this.numJugadores = numJugadores;
+	}
+
+	public Jugador[] getJu() {
+		return ju;
+	}
+
+	public void setJu(Jugador[] ju) {
+		this.ju = ju;
+	}
+
+	public int getRonda() {
+		return ronda;
+	}
+
+	public void setRonda(int ronda) {
+		this.ronda = ronda;
+	}
+	
+	
+}
